a223648402dc77ce952acbb609447b21e0089831
Romain Chambonnier <romain.chambonnier+git@protonmail.com>
2020-07-03 21:52:41 +0200

    using logger

--- mod.ts
+++ mod.ts
@@ -1,22 +1,28 @@
+import * as log from "https://deno.land/std/log/mod.ts";
+
+await log.setup({
+    handlers: {
+        console: new log.handlers.ConsoleHandler("DEBUG"),
+    },
+    loggers: {
+        default: {
+            level: "DEBUG",
+            handlers: ["console"],
+        }
+    }
+});
+
 async function downloadLaunchData() {
+    log.info("Downloading launch data...");
     const response = await fetch("https://api.spacexdata.com/v4/launches", { method: "GET" });
+
+    if(!response.ok) {
+        log.warning("Problem downloading launch data");
+        throw new Error("Launch data download failed.");
+    }
+
     const launchData = await response.json()
-    console.log(launchData);
+    //console.log(launchData);
 }
 
 await downloadLaunchData();
-
-async function addUser() {
-    const response = await fetch("https://reqres.in/api/users", {
-        method: "POST",
-        body: JSON.stringify({
-            name: "Elon Musk",
-            job: "billionaire"
-        }),
-        headers: {"Content-Type": "application/json; charset=UTF-8"}
-    });
-    const userInfos = await response.json();
-    console.log(userInfos);
-}
-
-await addUser();
