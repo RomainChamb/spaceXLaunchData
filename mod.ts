fcc0a28325f32b46976752a5a2227ded896a87cb
Romain Chambonnier <romain.chambonnier+git@protonmail.com>
2020-07-03 21:28:47 +0200

    using fetch to do a GET request

---
+++ mod.ts
@@ -0,0 +1,7 @@
+async function downloadLaunchData() {
+    const response = await fetch("https://api.spacexdata.com/v4/launches", { method: "GET" });
+    const launchData = await response.json()
+    console.log(launchData);
+}
+
+await downloadLaunchData();
