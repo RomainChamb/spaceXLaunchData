b0df1997d75551b181e8bfae877cd945431018d1
Romain Chambonnier <romain.chambonnier+git@protonmail.com>
2020-07-03 22:15:16 +0200

    storing result in map

--- mod.ts
+++ mod.ts
@@ -12,6 +12,13 @@ await log.setup({
     }
 });
 
+interface Launch {
+    flightNumber: number;
+    mission: string;
+}
+
+const launches = new Map<number, Launch>();
+
 async function downloadLaunchData() {
     log.info("Downloading launch data...");
     const response = await fetch("https://api.spacexdata.com/v4/launches", { method: "GET" });
@@ -22,7 +29,16 @@ async function downloadLaunchData() {
     }
 
     const launchData = await response.json()
-    //console.log(launchData);
+
+    for(const launch of launchData) {
+        const flightData = {
+            flightNumber: launch["flight_number"],
+            mission: launch["name"]
+        };
+        launches.set(flightData.flightNumber, flightData);
+
+        log.info(JSON.stringify(flightData));
+    }
 }
 
 await downloadLaunchData();
